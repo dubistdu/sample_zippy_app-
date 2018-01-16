e1268c2f7c23a7a5acb8d16cd1024f7622da007f
mama frantz <hyunfrantz@gmail.com>
2018-01-16 16:43:10 -0500

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+  users = User.create([{ name: 'Jasmine', role: 'dev' }, { name: 'Gavin', role: 'Sr dev' }, { name: 'Jason', role: 'CTO'}])
+  samples = Sample.create([
+    { name: 'Big Bird', gender: 'F', age: "20", phone_number: "111-1111", user_id: 1 },
+      name: 'Kermit', gender: 'M', age: "25", phone_number: "222-2222", user_id: 2},
+      name: 'Miss Piggy', gender: 'F', age: "30", phone_number: "333-3333", user_id: 3},
+    ])
